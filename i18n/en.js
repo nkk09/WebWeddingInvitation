74a4302c807b4ed332c32c6baa516992e1b8ee46
nkk09 <nkk09@mail.aub.edu>
2025-05-25 17:52:25 +0300

    update arabic translation

--- i18n/en.js
+++ i18n/en.js
@@ -13,6 +13,7 @@ window.translations_en = {
   verse: "\"So they are no longer two, but one. Therefore what God has joined together, let man not separate.\"",
   author: "Matthew 19:6",
   blessing: "with a heart full of joy,",
+  family:"the Boulos &amp; Chedid families",
   invite:"request the honor of your presence at the wedding of their son and daughter",
   couple: "Tony &amp; Sandy",
   date: "on Saturday, August 2<sup>nd</sup>, 2025",
